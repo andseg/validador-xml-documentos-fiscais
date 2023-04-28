77669b1d0b19f7b68552e4bb261ec0063bd9ecb3
FelpsFranco <felipefranco.morg@outlook.com>
2023-04-28 17:38:20 -0300

    Inserindo Descrição Settings

--- settings/settings.py
+++ settings/settings.py
@@ -103,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+# Alteração Realizada Felps
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/SaoPaulo'
